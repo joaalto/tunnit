60cfdb63841cf6cc36bc34b9edb206084fd01936
Joonas Aalto <joaalto@iki.fi>
2014-12-08 22:33:42 +0200

    Print day map keys.

--- src/cljs/tunnit/core.cljs
+++ src/cljs/tunnit/core.cljs
@@ -1,15 +1,37 @@
 (ns tunnit.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs-time.core :as time]
+            [om-tools.dom :as ot :include-macros true]
+            [om-tools.core :refer-macros [defcomponent]]
+            ))
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(enable-console-print!)
+
+(defonce app-state (atom {:text "Viikko"}))
+
+(def week-map
+  {:mon [] :tue [] :wed [] :thu [] :fri [] :sat [] :sun []})
+
+(defcomponent week-view [app owner]
+              (render [this]
+                      (ot/div
+                        (map (fn [day]
+                               (ot/output (str day)))
+                             (keys week-map))
+                        ))
+              )
+
+(defcomponent app-view [app owner]
+              (render [this]
+                      (ot/div {:class "column-main"}
+                              (dom/h1 nil (:text app))
+                              (om/build week-view app)
+                              )
+                      ))
 
 (defn main []
   (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (:text app)))))
+    app-view
     app-state
     {:target (. js/document (getElementById "app"))}))
