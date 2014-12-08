4ee7049498efbb24711885d2d8096cdd59363227
Joonas Aalto <joaalto@iki.fi>
2014-12-08 22:26:34 +0200

    Add cljs-time and om-tools.

--- project.clj
+++ project.clj
@@ -16,7 +16,10 @@
                  [environ "1.0.0"]
                  [com.cemerick/piggieback "0.1.3"]
                  [weasel "0.4.0-SNAPSHOT"]
-                 [leiningen "2.5.0"]]
+                 [com.andrewmcveigh/cljs-time "0.2.4"]
+                 [leiningen "2.5.0"]
+                 [prismatic/om-tools "0.3.6"]
+                 ]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-environ "1.0.0"]]
