8970be4899e790f41920ff446a40aa6250a53b10
Joonas Aalto <joaalto@iki.fi>
2014-12-10 00:11:40 +0200

    Print day names, show today's date.

--- src/cljs/tunnit/core.cljs
+++ src/cljs/tunnit/core.cljs
@@ -2,30 +2,40 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs-time.core :as time]
+            [cljs-time.format :as format]
             [om-tools.dom :as ot :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
             ))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Viikko"}))
+(def app-state (atom {:text "Viikko" :date ""}))
 
 (def week-map
-  {:mon [] :tue [] :wed [] :thu [] :fri [] :sat [] :sun []})
+  {:mon ["Ma"] :tue ["Ti"] :wed ["Ke"] :thu ["To"] :fri ["Pe"] :sat ["La"] :sun ["Su"]})
+
+(def date-format
+  (format/formatter "dd.MM.yyyy"))
+
+(defn today []
+  (format/unparse date-format (time/now)))
 
 (defcomponent week-view [app owner]
               (render [this]
+                      (om/update! app :date (today))
+
                       (ot/div
                         (map (fn [day]
-                               (ot/output (str day)))
-                             (keys week-map))
+                               (ot/output {:class "day-label"} (str (first day))))
+                               (vals week-map))
                         ))
               )
 
 (defcomponent app-view [app owner]
               (render [this]
+                      (print (format/show-formatters))
                       (ot/div {:class "column-main"}
-                              (dom/h1 nil (:text app))
+                              (dom/h1 nil (str (:text app) " " (:date app)))
                               (om/build week-view app)
                               )
                       ))
